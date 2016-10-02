a10cfdb70276c4d0036a3532b36144ca4b00e95c
cmgiler <cmgiler@gmail.com>
2016-10-01 22:06:54 -0400

    Finish outlier mini project

---
+++ 5.Identify-Fraud-from-Enron-Email/MiniProjects/outliers/outlier_cleaner.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+
+
+def outlierCleaner(predictions, ages, net_worths):
+    """
+        Clean away the 10% of points that have the largest
+        residual errors (difference between the prediction
+        and the actual net worth).
+
+        Return a list of tuples named cleaned_data where 
+        each tuple is of the form (age, net_worth, error).
+    """
+    import numpy as np
+    cleaned_data = []
+
+    ### your code goes here
+    errors = np.square(predictions - net_worths)
+    max_error = np.percentile(errors, 90)
+    for i in range(len(errors)):
+        if errors[i] < max_error:
+            cleaned_data.append((ages[i], net_worths[i], errors[i]))
+    
+    return cleaned_data
+
